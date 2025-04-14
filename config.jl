e27a209773785826b0a3d5634a379f3a5eaad5b7
ecerpac <ecerpac@student.tudelft.nl>
2025-04-14 18:19:27 +0200

    Add config.jl for path management; add outputs of 2D transformer example

---
+++ config.jl
@@ -0,0 +1,41 @@
+# Configuration file for the Future Distribution Systems AM project
+# This file defines common paths and settings that can be imported in notebooks
+
+"""
+Get standard project paths relative to a specified working directory.
+If no working directory is specified, uses the directory containing this file.
+"""
+function get_project_paths(working_dir=nothing)
+    root_dir = dirname(@__FILE__)
+    if isnothing(working_dir)
+        # Default to directory containing this config file
+        base_dir = root_dir
+    else
+        base_dir = joinpath(root_dir, working_dir)
+    end
+    
+    # Main project directories
+    paths = Dict(
+        "ROOT_DIR" => root_dir,
+        "BASE_DIR" => base_dir,
+        "TA_EXAMPLE_1D_DIR" => joinpath(root_dir, "ta_example_1d"),
+        "TA_EXAMPLE_2D_DIR" => joinpath(root_dir, "ta_example_2d_distribution_transformer"),
+        "REPORT_DIR" => joinpath(root_dir, "report")
+    )
+    
+    # Sub-directories
+    paths["GEO_DIR"] = joinpath(paths["BASE_DIR"], "geo")
+    paths["REPORT_FIGURE_DIR"] = joinpath(paths["REPORT_DIR"], "Figures")
+    paths["OUTPUT_DIR"] = joinpath(paths["BASE_DIR"], "output")
+    
+    # Create directories if they don't exist
+    for (_, path) in paths
+        mkpath(path)
+    end
+    
+    return paths
+end
+
+
+# Default exports when including this file
+export get_project_paths
